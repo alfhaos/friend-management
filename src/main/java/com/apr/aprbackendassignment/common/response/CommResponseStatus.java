4328742c0f022649489f51d3acd62931a3ea310b
alfhaos <alfhaos@naver.com>
2026-01-23 19:56:58 +0900

    [refactor] API 규약에 따른 HTTP 상태 코드 적정화

--- src/main/java/com/apr/aprbackendassignment/common/response/CommResponseStatus.java
+++ src/main/java/com/apr/aprbackendassignment/common/response/CommResponseStatus.java
@@ -22,16 +22,17 @@ public enum CommResponseStatus {
     // Success
     SUCCESS(HttpStatus.OK, "SUCCESS", "Success"),
 
-    // Common
+    // 4xx Client Error
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "Bad Request"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "Not Found"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "Internal Server Error"),
-    NOT_FOUND_USER(HttpStatus.INTERNAL_SERVER_ERROR, "NOT_FOUND_USER", "해당 사용자를 찾을수 없습니다."),
-
-    // Friendship
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "NOT_FOUND_USER", "해당 사용자를 찾을수 없습니다."),
     SELF_FRIEND_REQUEST(HttpStatus.BAD_REQUEST, "SELF_FRIEND_REQUEST", "자기 자신에게는 친구 요청을 보낼 수 없습니다."),
     ALREADY_REQUESTED_FRIEND(HttpStatus.CONFLICT, "ALREADY_REQUESTED_FRIEND", "이미 친구 요청이 존재합니다."),
-    TOO_MANY_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "TOO_MANY_REQUEST", "요청이 너무 많습니다. 잠시 후 다시 시도해주세요.");
+    TOO_MANY_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "TOO_MANY_REQUEST", "요청이 너무 많습니다. 잠시 후 다시 시도해주세요."),
+    NOT_FOUND_FRIENDSHIP(HttpStatus.NOT_FOUND, "NOT_FOUND_FRIENDSHIP", "해당 친구관계를 찾을수 없습니다."),
+    // 5xx Server Error
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "Internal Server Error");
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
